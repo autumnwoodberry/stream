0a5e22d7ee09dd0af36300be2830196ac5e0dc53
Adam Woodbury <adamwoodbury@gmail.com>
2015-08-15 13:34:43 -0700

    Render version number in template from package.json

--- visualizer/views.py
+++ visualizer/views.py
@@ -1,5 +1,16 @@
+from django.conf import settings
 from django.shortcuts import render
-
+import json
+import os
 
 def index(request):
-    return render(request, 'visualizer/index.html')
+
+    context = {
+        'version': 'null'
+    }
+
+    with open(os.path.join(settings.BASE_DIR, 'package.json')) as package:
+        info = json.load(package)
+        context['version'] = info.get('version', 'null')
+
+    return render(request, 'visualizer/index.html', context)
